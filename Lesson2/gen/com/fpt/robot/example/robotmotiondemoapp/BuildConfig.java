3ac39da13ad8e3fa98e0447e79ee912f29433048
ftifpt <fti.fpt.ho@gmail.com>
2013-07-01 16:11:40 +0700

    initial

---
+++ Lesson2/gen/com/fpt/robot/example/robotmotiondemoapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.fpt.robot.example.robotmotiondemoapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
